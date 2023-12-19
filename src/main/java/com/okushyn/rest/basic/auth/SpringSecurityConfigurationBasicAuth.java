2e971bbe23c8ddb8256e179c5aabff7f96afda5f
kyshin <kyshin1993@gmail.com>
2023-12-19 19:28:19 +0100

    disabling of basic security (take out from package)

--- src/main/java/com/okushyn/rest/basic/auth/SpringSecurityConfigurationBasicAuth.java
+++ src/main/java/com/okushyn/rest/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -1,4 +1,4 @@
-package com.okushyn.rest.webservices.restfulwebservices.basic.auth;
+package com.okushyn.rest.basic.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
